cdad1cf5bf83ec2ed5d3f865e029b58bde29b18b
TAEHEE <jangth0655@gmail.com>
2024-01-02 22:32:44 +0900

    firebase download and initial setting

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAh2tmRC0jRA29eiXY1PnKgHSrNrLmXgUY',
+  authDomain: 'fir-basic-e4357.firebaseapp.com',
+  projectId: 'fir-basic-e4357',
+  storageBucket: 'fir-basic-e4357.appspot.com',
+  messagingSenderId: '802952766073',
+  appId: '1:802952766073:web:d952ff7501abc139a62aa8',
+  measurementId: 'G-LKTBHB58DS',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
